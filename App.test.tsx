30f51fffa15662dfe219501633bf5c69b714ec9a
Andrew Glago <andrewglago1@gmail.com>
2022-01-19 22:08:23 +0000

    cleanup config

--- App.test.tsx
+++ App.test.tsx
@@ -3,6 +3,6 @@ import 'react-native';
 import { render } from '@testing-library/react-native';
 import App from './App';
 
-it('renders correctly', () => {
+it('renders without crashing', () => {
   render(<App />);
 });
